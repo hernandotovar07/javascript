92c8d42d6119c5a8acae14d6fc2cdede4fecfd39
hernandotovar07 <hernandotovar2000@gmail.com>
2024-10-17 14:30:28 -0500

    test

---
+++ js/clase1-1.js
@@ -0,0 +1,4 @@
+function sumar (a, b){
+    return a + b;
+}
+let resultado = sumar(5, 3);
---
+++ variable.js
@@ -0,0 +1,80 @@
+
+let mensaje = "hola mundo"
+
+console.log(mensaje)
+
+mensaje = "chao mundo"
+console.log(mensaje)
+
+const PI = 3.1415
+console.log(PI)
+//PI = 8
+console.log(PI)
+
+let cantidad = 43;
+let texto = 'texto de prueba';
+let gameover = false;
+let variableIndefinida;
+console.log (cantidad);
+console.log (variableIndefinida, gameover);
+
+
+
+
+
+let lista = [3,6,8,9,3,7]
+console. log(lista[4])
+
+
+
+lista[10]=18
+console.log(lista)
+
+
+let persona = {
+    nombre:"hernando",
+    apellido:"tovar",
+    ocupacion:"biologo",
+    telefono:3042477234 ,
+    correro:"hernandotovar2000@gmail.com",
+}
+  console.log(persona.nombre)
+
+
+
+
+    function saludar() {
+        console.log("hola grupo")
+    }
+    saludar()
+
+
+
+    let resultado = 4+6
+    console. log(resultado)
+
+    let valor = 50
+    valor += 50
+    console.log(valor)
+    x = 0
+    x*=1
+    console.log(x)
+
+
+
+
+    let persona1 ="jorge"
+    let persona2 ="carlos"
+    let valor1 = 33
+    let valor2 = 45
+
+    console.log (persona1 == persona2)
+    console.log (persona1 != persona2)
+    console.log ( valor1 !== valor2)
+    console.log ( valor1 != valor2)
+
+    if(persona1 == persona2){
+        console.log("los valores son iguales")
+    }else{
+        console.log("los valores son diferentes")
+    }
